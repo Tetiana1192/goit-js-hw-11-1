8d6ce47c16013357d296385a41c067a0059cba15
Tetiana1192 <tanya_strigun@ukr.net>
2022-05-29 21:33:16 +0300

    t

--- src/index.js
+++ src/index.js
@@ -1,57 +1,89 @@
+import Notify from 'notiflix';
 
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import LoadMoreBtn from './js/load-moreBtn';
+import ImagesApiService from './js/imageApiService.js';
+import './sass/main.scss';
+import Markup from './js/gallerymerkup';
 
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-
-// import ImageApiService from './js/imageApiService.js';
-// import './sass/main.scss';
 
 const refs = {
   searchForm: document.querySelector('.search-form'),
     galleryEl: document.querySelector('.gallery'),
   loadMoreBtn:document.querySelector('.load-more')
 };
-const imageApiService =  new ImageApiService();   // отримуємо доступ до об`єктів та масівов/робимо екземпляр
+const imagesAPIService = new ImagesAPIService();  
+const loadMoreBtn = new LoadMoreBtn({ selector: '.load-more' });
+const renderMarkup = new Markup({ selector: refs.gallery });
 
 refs.searchForm.addEventListener('submit', onSearch);
-refs.loadMoreBtn.addEventListener('click', onLoadMore);
+loadMoreBtn.button.addEventListener('click', onloadMoreBtnClick);
 
 
 
-function onSearch(e) {
-    e.preventDefault();
-    // посилання на форму 
-    imageApiService.query = e.currentTarget.elements.query.value;
-    imageApiService.resetPage(); // скидання
-    imageApiService.fetchArticles().then(generateCardsMurkup);
+async function onSearch(e) {
+  e.preventDefault();
+  renderMarkup.reset();
+  imagesAPIService.query = e.currentTarget.searchQuery.value.trim();
+
+  if (imagesAPIService.query === '') {
+    loadMoreBtn.hideBtn();
+    Notify.info('Your query is empty. Try again!');
+    return;
+  }
+
+  imagesAPIService.resetPage();
+
+  try {
+    loadMoreBtn.showBtn();
+    await initFetchImages();
+  } catch (error) {
+    loadMoreBtn.hideBtn();
+    Notify.failure(error.message);
+  }
+
+  refs.form.reset();
 }
 
-function onLoadMore() {
-  imageApiService.fetchArticles().then(generateCardsMurkup);
+
+
+
+async function onloadMoreBtnClick() {
+  try {
+    await initFetchImages();
+  } catch {
+    Notify.failure(error.message);
+  }
+  pageScroll();
+  renderMarkup.lightbox.refresh();
 }
 
+async function initFetchImages() {
+  
+  try {
+    loadMoreBtn.disable();
+    const images = await imagesAPIService.fetchImages();
+    renderMarkup.items = images;
+    renderMarkup.render();
+  } catch {
+    Notify.failure(error.message);
+  }
+   
 
-function generateCardsMurkup(articles) {
-  return cardsArray
-    .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-      return `<a href="${largeImageURL}" class="photo-card">
-        <img src="${webformatURL}" alt="${tags}" class="photo-card__img" width="300" loading="lazy" />
-        <div class="info">
-          <p class="info-item"><b>Likes</b><br />${likes}</p>
-          <p class="info-item"><b>Views</b><br />${views}</p>
-          <p class="info-item"><b>Comments</b><br />${comments}</p>
-          <p class="info-item"><b>Downloads</b><br />${downloads}</p>
-        </div>
-      </a>`;
-    })
-    .join('');
+  if (imagesAPIService.endOfHits) {
+    loadMoreBtn.hideBtn();
+    return;
+  }
+  loadMoreBtn.enable();
 }
 
-function appendCardsMurkup(articles) {
-  gallery.insertAdjacentHTML('beforeend', generateCardsMurkup(articles));
-}
+function pageScroll() {
+    const { height: formHeight } = refs.form.getBoundingClientRect();
+    const { height: cardHeight } = refs.gallery.firstElementChild.getBoundingClientRect();
 
-function resetGallery() {
-  gallery.innerHTML = '';
+    window.scrollBy({
+        top: cardHeight * 2 - formHeight * 2,
+        behavior: 'smooth',
+    });
 }
